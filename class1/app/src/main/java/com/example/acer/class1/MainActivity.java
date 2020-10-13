9ffe39c692bd126f2877a874ad182b03548d3bcd
moten-07 <58394845+moten-07@users.noreply.github.com>
2020-10-14 00:43:40 +0800

    Update MainActivity.java

--- class1/app/src/main/java/com/example/acer/class1/MainActivity.java
+++ class1/app/src/main/java/com/example/acer/class1/MainActivity.java
@@ -6,8 +6,8 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {    //相当于main方法
+    protected void onCreate(Bundle savedInstanceState) {    //活动，相当于main方法
         super.onCreate(savedInstanceState);                 //继承父类方法
-        setContentView(R.layout.activity_main);            //设置内容的布局
+        setContentView(R.layout.activity_main);            //绑定内容的布局
     }
 }
