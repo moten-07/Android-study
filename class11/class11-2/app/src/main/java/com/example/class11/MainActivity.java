c80e8ab42f80524c796876300d081cf6d4527cfb
moten-07 <58394845+moten-07@users.noreply.github.com>
2020-10-26 09:33:54 +0800

    moten

--- class11/class11-2/app/src/main/java/com/example/class11/MainActivity.java
+++ class11/class11-2/app/src/main/java/com/example/class11/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.class11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.Recy1);
+
+    }
+
+    //加载数据初始化
+    public void intiData(){
+        list=new ArrayList<String>();
+        for (int i=1;i<=30;i++){
+            String item="item"+i;
+            list.add(item);
+        }
     }
 }
